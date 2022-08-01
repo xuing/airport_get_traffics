dbe1ff766bf58f2a021ae79a0940efc317d16bf4
xuing <admin@xuing.cn>
2022-08-02 06:15:40 +0800

    update failed logs

--- kcjisu_get_traffics.py
+++ kcjisu_get_traffics.py
@@ -1,9 +1,11 @@
 import os
 import cloudscraper
 
+
 def login(email, password):
     headers = {
-        "User-Angent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+        "User-Angent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                       "Chrome/88.0.4324.190 Safari/537.36",
         "accept": "application/json, text/javascript, */*; q=0.01",
         "accept-language": "zh,zh-CN;q=0.9,en;q=0.8,ja;q=0.7",
         "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -18,12 +20,15 @@ def login(email, password):
         "passwd": password,
     }
     s = cloudscraper.create_scraper()
-    s.post(url=login_url, data=login_data, headers=headers)
+    res = s.post(url=login_url, data=login_data, headers=headers)
+    print(res.json())
     return s
+
 
 def get_traffic(s):
     headers = {
-        "User-Angent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+        "User-Angent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                       "Chrome/88.0.4324.190 Safari/537.36",
         "accept": "application/json, text/javascript, */*; q=0.01",
         "accept-language": "en-US,en;q=0.9",
         "sec-fetch-dest": "empty",
@@ -31,10 +36,13 @@ def get_traffic(s):
         "sec-fetch-site": "same-origin",
         "x-requested-with": "XMLHttpRequest"
     }
-    gourl = "https://"+os.environ["KCJISU_DOMAIN"]+"/user/checkin"
-    res = s.post(url=gourl, headers=headers,verify=False)
-    print(res)
-    print(res.json())
+    gourl = "https://" + os.environ["KCJISU_DOMAIN"] + "/user/checkin"
+    res = s.post(url=gourl, headers=headers, verify=False)
+    print("get_traffic status: ", res)
+    try:
+        print(res.json())
+    except:
+        print("get_traffic error, text: ", res.text)
 
 
 if __name__ == "__main__":
